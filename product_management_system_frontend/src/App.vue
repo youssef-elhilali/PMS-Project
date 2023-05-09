4a20b69154079df3192cc892d5f5ced9182e0479
youssef24hilali <y.elhilali24@gmail.com>
2023-05-09 15:45:10 +0100

    pms-first-commit

---
+++ product_management_system_frontend/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <router-view></router-view>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  components: {
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+}
+
+label{
+  font-weight: 600;
+  color: #797979;
+}
+
+.error-feedback{
+  color: #dc3545;
+}
+
+</style>
